6086f33370966b2a33567b8189d4c5f42ed19069
HelloDearSir <jonbarton@hotmail.co.uk>
2019-04-05 15:49:36 +0100

    from hci unit
    This is from the HCI unit

---
+++ hitman/js/script.js
@@ -0,0 +1,47 @@
+ function myfunction() { 
+ window.location.href = "index.html";
+} 
+
+function setupfunction()
+{
+window.location.href = "setup.html";
+}
+
+function ytsong() {
+ window.location.href = "https://www.youtube.com/watch?v=2pCJhDey9XI";
+}
+
+function  guidelinesf() 
+{ 
+window.location.href = " https://docs.google.com/document/d/1BNmgFWRh1gtZRkpWG40bF2R54SncXsky2hsvIDEuqd0/edit?usp=sharing";
+}
+
+function feedback() {
+
+ window.location.href = " https://docs.google.com/forms/d/e/1FAIpQLSfJZSLDdk_uIMN3YMXIgUmRTBh5UjwZFKw3LsRzXFabAtyBWA/viewform?usp=sf_link"; 
+}
+
+function nextbutton() {
+ window.location.href = " pay.html" ;
+} 
+
+function death() {
+	window.location.href = "death.html";
+}
+
+function upload() {
+window.location.href = "upload.html";	
+
+}
+
+function date() {
+	window.location.href = " date.html"; 
+}
+
+function location(){
+	window.location.href = " location.html";
+}
+	function music() {
+       var audio = document.getElementById("audiot");
+       audio.play();
+	}
